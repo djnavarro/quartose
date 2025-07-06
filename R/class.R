5d0af8ca7fd52979d17d51a49cc74f3e5d03208d
Danielle Navarro <djnavarro@protonmail.com>
2025-07-06 16:32:03 +1000

    improves class documentation

--- R/class.R
+++ R/class.R
@@ -6,23 +6,101 @@
 #' Intended to be used inside a quarto document, within a knitr 
 #' code chunk with the `results: asis` option set.
 #'   
-#' @param content Tab content as a list.
-#' @param title Section title, a character string.
-#' @param level Header level, an integer between 1 and 6.
-#' @param names Names for tabs (defaults to names(content)).
-#' @param class CSS class (or vector of classes) to be supplied.
-#' @param sep Separator used when merging content.
+#' @param content 
+#' List or character vector containing content to be included within 
+#' the quarto object. The expected format of the `content` argument 
+#' differs slightly depending on which function is used. See the 
+#' "details" section for more information.
+#' 
+#' @param title 
+#' Character string specifying the text to use as a section title. 
+#' For `quarto_section()` this is a required argument. For `quarto_tabset()` 
+#' it is permitted to use `title = NULL`, in which case the tabset will 
+#' be printed without a section header above it. This is the default 
+#' behaviour for tabsets.
+#' 
+#' @param level 
+#' Numeric header level applied to section title or tabset names. 
+#' The `level` argument must be a whole number between 1 and 6. Only 
+#' relevant to quarto objects that produce section headings, specifically 
+#' `quarto_section()` and `quarto_tabset()`.
+#' 
+#' @param names 
+#' Character vector of names to be applied to the tabs in a tabset. Only 
+#' relevant to  `quarto_tabset()`. If `names = NULL`, the names will be 
+#' taken from the names of the `content` argument.
+#' 
+#' @param class 
+#' Character vector specifying CSS classes to be applied to the content. 
+#' Only relevant to `quarto_div()` and `quarto_span()`. Defaults to 
+#' `class = NULL`, in which case the formatted text written to the document 
+#' will have a dummy CSS class "quartose-null" applied.
+#' 
+#' @param sep 
+#' Character string specifying the separator to be used when merging content
+#' for printing to the document. Defaults to `sep = ""` for all functions.
 #'  
 #' @return 
 #' These functions always return an object with parent S3 class 
 #' "quarto_object", in addition to a specific S3 class corresponding
-#' to the function (e.g., `quarto_section()` objects also possess the
-#' "quarto_section" class). When printed, the object is
-#' rendered to a character vector of length 1 containing the quarto 
-#' syntax that inserts an HTML section header into the document.
+#' to the function. For example, `quarto_section()` objects also possess the
+#' "quarto_section" class.
 #' 
 #' @details
-#' Additional details...
+#' The purpose of these functions is to allow the user to dynamically 
+#' generate quarto syntax from R. When used within a quarto document they
+#' allow the user to generate callouts, margin text, tabsets, section 
+#' headers, and other kinds of quarto output. At the current state of 
+#' development the functionality is somewhat limited, discussed below.
+#' 
+#' The `quarto_*()` functions supplied by the quartose package have a common
+#' design: argument values supplied by the user are stored internally as a 
+#' list, with only a minimum of processing done at the time that the function
+#' is called. The object is assigned to two S3 classes, the "quarto_object"
+#' shared by all objects, and a specific class associated with the calling 
+#' function. These objects can be inspected and manipulated programmatically
+#' like any other R objects prior to printing.
+#' 
+#' When creating a quarto object, note that most `quarto_*()` functions take 
+#' a `content` argument, which differs slightly depending on the context:
+#' 
+#' - For `quarto_section()` there is no `content`` argument: section headers 
+#'   have titles, but they do not contain content.
+#' - For `quarto_span()` the `content`` argument *must* be a character vector, 
+#'   not a list.
+#' - For `quarto_div()` the `content`` argument is permitted to be a character
+#'   vector or a list, but it will always be stored internally as a list. 
+#'   If the input is a list, it can contain other quarto objects. The 
+#'   intended use for this is a div that contains several spans, but it is
+#'   not limited to this use case.
+#' - For `quarto_tabset()` the `content` argument *must* be a list. The list 
+#'   elements can be any printable R object: each element of the list will
+#'   appear in its own tab. At present the support for graphics objects is
+#'   limited: ggplot2 objects are captured and will only be rendered when
+#'   `knitr::knit_print()` is called. No attempt is made (as yet!) to support
+#'   other kinds of graphic objecvts, and if these are passed via the `content`
+#'   argument the function will likely fail.
+#' - For `quarto_markdown()` the `content` argument may be a character vector
+#'   or a list of character vectors. The function will throw an error if other
+#'   kinds of objects are passed via `content`.
+#' - For `quarto_group()` the `content` argument *must* be a list, and all 
+#'   elements of the list must be quarto objects. The intended use of this 
+#'   function is simply to collect several quarto objects into a single group
+#'   that will be printed all at the same time rather than sequentially.
+#' 
+#' Creating a quarto object only defines the data structure, it does not 
+#' perform any formatting. Similarly, if the object is printed using 
+#' `print()`, no formatting will be applied. A brief summary of the 
+#' data structure will be printed to the console, no more. However, when
+#' `knitr::knit_print()` is called, the quarto object is first passed to
+#' the relevant `format()` method, which is responsible for constructing 
+#' the appropriate quarto syntax. Calling `format()` will return a
+#' character vector or a list. If it returns a list all elements will 
+#' either be character strings with the appropriate quarto syntax, or a
+#' plot object that has not yet been rendered. After formatting is applied
+#' the `knitr::knit_print()` method will pass the strings (or plots) to 
+#' the document. For more detail on the formatting and printing methods 
+#' see `knit_print.quarto_object()` and `format.quarto_object()`.
 #' 
 #' @name quarto_object
 #' 
@@ -35,44 +113,75 @@
 #' quarto_markdown
 #' 
 #' @examples
-#' # a quarto_section object
+#' # quarto_section ------------------------------------------------------
+#' 
 #' sec <- quarto_section("A level-two header", level = 2L)
+#' 
+#' # quarto objects have two classes, a general purpose class shared by 
+#' # all quarto objects, and a class specific to the function
+#' class(sec) 
 #'  
-#' # base::print() displays a summary of the object 
+#' # base::print() displays an abstract summary of the object 
 #' print(sec)
 #' 
-#' # knitr::knit_print() displays the rendered quarto syntax
-#' knitr::knit_print(sec) 
+#' # knitr::knit_print() produces the rendered quarto syntax
+#' knitr::knit_print(sec)
+#' 
+#' # quarto_span ---------------------------------------------------------
 #'
-#' # a quarto_span object
-#' spn <- quarto_span("This is underlined", class = "underline")
+#' spn1 <- quarto_span("This is plain text")
+#' spn2 <- quarto_span("This is underlined text", class = "underline")
 #' 
-#' print(spn)
+#' print(spn1)
 #' 
-#' knitr::knit_print(spn)
+#' print(spn2)
 #' 
-#' # a quarto_div object 
-#' div <- quarto_div("This is a callout note", class = "callout-note")
+#' knitr::knit_print(spn1)
 #' 
-#' print(div)
+#' knitr::knit_print(spn2)
 #' 
-#' knitr::knit_print(div)
+#' # quarto_div ----------------------------------------------------------
 #' 
-#' # a quarto_tabset object
+#' # quarto_div objects are flexible: they can take a character vector as
+#' # the content argument, but can also take lists of other objects; note
+#' # that internally the content is always represented as a list
+#' div1 <- quarto_div("This is a callout note", class = "callout-note")
+#' div2 <- quarto_div(
+#'   content = list(
+#'     quarto_span(content = "You can wrap multiple spans in a div so that"),
+#'     quarto_span(content = "some text is highlighted", class = "mark"),
+#'     quarto_span(content = "and some is underlined", class = "underline")
+#'   ),
+#'   class = c("column-margin", "callout-tip"),
+#'   sep = " "
+#' )
+#' 
+#' print(div1)
+#' 
+#' print(div2)
+#' 
+#' knitr::knit_print(div1)
+#' 
+#' knitr::knit_print(div2)
+#' 
+#' # quarto_tabset -------------------------------------------------------
+#' 
 #' tbs <- quarto_tabset(list(tab1 = 1:10, tab2 = "hello"), level = 3L)
 #' 
 #' print(tbs)
 #' 
 #' knitr::knit_print(tbs)
 #' 
-#' # a quarto_markdown object
+#' # quarto_markdown -----------------------------------------------------
+#' 
 #' mkd <- quarto_markdown(list("- a markdown", "- list"), sep = "\n")
 #' 
 #' print(mkd)
 #' 
 #' knitr::knit_print(mkd)
 #' 
-#' # a quarto_group object
+#' # quarto_group --------------------------------------------------------
+#' 
 #' grp <- quarto_group(list(
 #'   quarto_div("This is a callout note", class = "callout-note"),
 #'   quarto_div("This is a callout tip", class = "callout-tip")
